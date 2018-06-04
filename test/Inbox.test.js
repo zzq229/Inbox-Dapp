244b3521a5fc931a173a36e67f322b788a86c3f2
zzq229 <zzqwaterloo@gmail.com>
2018-06-04 02:12:08 -0400

    add full coverage test in local eth network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,24 +1,43 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
 let accounts;
 
+let inbox;
+
 beforeEach(async () => {
 	// Get list of all accounts 
 	accounts = await web3.eth.getAccounts();
-		// .then(fetchedAccounts => {
-		// 	console.log(fetchedAccounts)
-		// });
+	
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 
 
 	// Use one to depoly contract
 });
 
 describe('Inbox', () => {
-	it('depoly a contract', () => {
-		console.log(accounts)
-	});
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+
 });
